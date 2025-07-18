7cab416bb150bce1777cad47dcdf497c62fd9949
Jithin Binoy <jithinbinoy2000@gmail.com>
2025-07-18 14:49:47 +0530

    Server url updated for production

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,1 +1,2 @@
-export const serverUrl ="http://localhost:3000/api"
+// export const serverUrl ="http://localhost:3000/api"
+export const serverUrl = "https://student-management-system-backend-ilsa.onrender.com/api"
